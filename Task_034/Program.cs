df4d96a2851775c696f65d64edb8013de8d69a5b
«Kulikov <shouter84@yandex.ru>
2023-05-23 14:16:32 +0300

    Added solution for Task_034 and Task_036

---
+++ Task_034/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34:
+
+// Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, которая покажет количество
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return tempArray;
+}
+
+int CountPositiveElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+int[] arr = FillArray(10, 100, 999);
+PrintArray(arr);
+System.Console.WriteLine($"Number of even numbers in array: {CountPositiveElements(arr)}");
---
+++ Task_036/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 36: 
+
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return tempArray;
+}
+
+int CountPositiveElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+int[] arr = FillArray(7, -20, 100);
+PrintArray(arr);
+System.Console.WriteLine($"Sum of number with odd index in array: {CountPositiveElements(arr)}");
